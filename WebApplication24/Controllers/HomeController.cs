d05e573cd9a7324fdcd3f54444e6aeb47aa07803
Rochel Schulgasser <rochelschulgasser@gmail.com>
2022-03-16 13:50:32 -0400

    Made a change to my controller

--- WebApplication24/Controllers/HomeController.cs
+++ WebApplication24/Controllers/HomeController.cs
@@ -11,13 +11,9 @@ namespace WebApplication24.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             return View();
